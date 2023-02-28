5ed2458a5a6df7c2fb11bfe26d2d6b0b69b6403b
Julien de Lambilly <julien.delambilly@gmail.com>
2023-02-28 14:11:03 +0100

    add model rental with validation and migration file

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :rental
+  belongs_to :user, through: :rental
+  validates :rating, presence: true, numericality: {
+    only_integer: true, greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
+  validates :content, presence: true
+end
