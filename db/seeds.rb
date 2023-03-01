81289bbb27cd8a9be7d6e4c634d6424c2598078d
Thu Nguyen <thunguyen.biz.ch@gmail.com>
2023-03-01 15:40:23 +0100

    finished view starship action show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 require 'faker'
 require 'open-uri'
-# puts 'cleaning database'
-# Review.destroy_all
-# Rental.destroy_all
-# Starship.destroy_all
-# User.destroy_all
+
+puts 'cleaning database'
+Review.destroy_all
+Rental.destroy_all
+Starship.destroy_all
+User.destroy_all
 
 puts 'creating users'
 5.times do
