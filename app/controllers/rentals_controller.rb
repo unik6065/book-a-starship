64fcc3a35bfa64aedf975dadbbfa8b01d1a6610b
Julien de Lambilly <julien.delambilly@gmail.com>
2023-03-01 14:19:42 +0100

    create rental et index rental fonctionne

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,22 +3,24 @@ class RentalsController < ApplicationController
 
   # Index only shows rentals for the current user
   def index
-    @rentals = current_user.rentals
+    @rentals = Rental.where(renter_id: current_user.id)
   end
 
   def new
     @rental = Rental.new
+    @starship = Starship.find(params[:starship_id])
   end
 
   def create
     @rental = Rental.new(rental_params)
-    @rental.renter = current_user
-    @rental.starship = Starship.find(params[:starship_id])
+    @rental.renter_id = current_user.id
+    @rental.starship_id = params[:starship_id]
     # rental price is calculated by multiplying the price per day of the starship
     # by the number of days and is at minimum equal to the price per day
     @rental.price = [@rental.starship.price_per_day * (@rental.end_date - @rental.start_date).to_i, @rental.starship.price_per_day].max
+    puts @rental.valid?
     if @rental.save
-      redirect_to rental_path(@rental)
+      redirect_to rentals_path
     else
       render :new, status: :unprocessable_entity
     end
