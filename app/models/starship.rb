13ecb3fa948c311556a093b76e60ccbc44ee37c6
romain isoz <isozromain@gmail.com>
2023-03-02 11:06:34 +0100

    modification merge

--- app/models/starship.rb
+++ app/models/starship.rb
@@ -10,19 +10,18 @@ class Starship < ApplicationRecord
   validates :description, presence: true
   validates :price_per_day, numericality: { only_numeric: true }, presence: true
 
-
   def average_rate
     sum = 0
     rentals.each do |rental|
       sum += rental.review.rating
     end
     (sum.to_f / rentals.length).truncate(1)
+  end
 
   def attach_photos
     return if photos.blank?
 
     @starship.attach(photos)
     self.new_photos = []
-
   end
 end
